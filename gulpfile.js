5f0ec26c18f0b521c6c010c942b2ff9a7196ce17
sandeep joshi <joshi.sandeep.d@gmail.com>
2015-12-18 21:32:41 +0530

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+/*=============================================
+=            Gulp Starter by @dope            =
+=============================================*/
+
+/**
+*
+* The packages we are using
+* Not using gulp-load-plugins as it is nice to see whats here.
+*
+**/
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var browserSync  = require('browser-sync');
+var prefix       = require('gulp-autoprefixer');
+var plumber      = require('gulp-plumber');
+var uglify       = require('gulp-uglify');
+var rename       = require("gulp-rename");
+var imagemin     = require("gulp-imagemin");
+var pngquant     = require('imagemin-pngquant');
+
+/**
+*
+* Styles
+* - Compile
+* - Compress/Minify
+* - Catch errors (gulp-plumber)
+* - Autoprefixer
+*
+**/
+gulp.task('sass', function() {
+  gulp.src('src/sass/**/*.sass')
+  .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(prefix('last 2 versions', '> 1%', 'ie 8', 'Android 2', 'Firefox ESR'))
+  .pipe(plumber())
+  .pipe(gulp.dest('dist/css'));
+});
+
+/**
+*
+* BrowserSync.io
+* - Watch CSS, JS & HTML for changes
+* - View project at: localhost:3000
+*
+**/
+gulp.task('browser-sync', function() {
+  browserSync.init(['dist/css/*.css', 'dist/js/**/*.js', 'index.html'], {
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+
+/**
+*
+* Javascript
+* - Uglify
+*
+**/
+gulp.task('scripts', function() {
+  gulp.src('src/js/*.js')
+  .pipe(uglify())
+  .pipe(rename({
+    dirname: "min",
+    suffix: ".min",
+  }))
+  .pipe(gulp.dest('dist/js'))
+});
+
+/**
+*
+* Images
+* - Compress them!
+*
+**/
+gulp.task('images', function () {
+  return gulp.src('src/images/*')
+  .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    use: [pngquant()]
+  }))
+  .pipe(gulp.dest('dist/images'));
+});
+
+
+/**
+*
+* Default task
+* - Runs sass, browser-sync, scripts and image tasks
+* - Watchs for file changes for images, scripts and sass/css
+*
+**/
+gulp.task('default', ['sass', 'browser-sync', 'scripts', 'images'], function () {
+  gulp.watch('src/sass/**/*.sass', ['sass']);
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('src/images/*', ['images']);
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Demo Page
+
+Used [starter kit](http://github.com/dope/gulp-starter.git) for front-end builds.
+
+### Getting Started
+Clone: ``
+
+Once you have cloned the repo, navigate in to the directory via terminal and run.
+
+```bash
+npm install
+gulp
+```
