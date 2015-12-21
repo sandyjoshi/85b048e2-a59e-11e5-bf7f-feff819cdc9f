6ed061ff68e6fda322646c0976b4dc46b627b302
sandeep joshi <joshi.sandeep.d@gmail.com>
2015-12-21 09:42:24 +0530

    media query for mobile and tablet

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,20 @@
-console.log('Hello World')
+
+$('.menu-button').on('click', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+	var items = $('.navbar-items');
+	items.toggleClass('close');
+});
+
+
+$('.contact-button').on('click', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+	$('.banner').toggle();
+	$('.sub-menu-section').toggle();
+	$('.main-section').toggle();
+	$('.series-section').toggle();
+	$('.article-section').toggle();
+	$('.side-bar').toggle();
+
+});
